1c9b4b53a76ecea4ce2fa66122d76a4c0fe454bd
wudong <wudong@uinnova.com>
2021-02-08 11:21:14 +0800

    feat: 后生可畏

--- Test/test6.js
+++ Test/test6.js
@@ -1,4 +1,15 @@
-const floor = THING.App.current.query('.Floor');
-floor.forEach(item => {
-    item.x = 0
-})
+function filterTree(tree, list) {
+    return tree.reduce((acc, { name, node_id, children }) => {
+        if (list.includes(node_id)) {
+            if (children && children.length) {
+                children = filterTree(children, list)
+            }
+            acc.push(JSON.parse(JSON.stringify({
+                name,
+                node_id,
+                children: (children && children.length) ? children : undefined
+            })))
+        }
+        return acc
+    }, [])
+};
